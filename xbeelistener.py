a6d92071a939b07cfac2494cf46e943babf76ff1
Michal Mrozik <michal.mrozik@gmail.com>
2015-01-10 20:06:24 +0100

    Porządkowanie

--- xbeelistener.py
+++ xbeelistener.py
@@ -4,29 +4,9 @@ from xbee import ZigBee
 import serial
 import logging.handlers
 import sqlite3
+import sys
 from time import sleep
 from datetime import  datetime
-
-
-LOG_FILENAME = "xbeelistener.log"
-LOG_LEVEL = logging.INFO # Could be e.g. "DEBUG" or "WARNING"
-
-ser = serial.Serial("/dev/ttyUSB0", baudrate=38400, timeout=1)
-xbee = ZigBee(ser,escaped=True)
-
-DB = '/home/michal/workspace/iDomKoordynator/dev.db'
-
-logger = logging.getLogger(__name__)
-# Set the log level to LOG_LEVEL
-logger.setLevel(LOG_LEVEL)
-# Make a handler that writes to a file, making a new file at midnight and keeping 3 backups
-handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=3)
-# Format each log message like this
-formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
-# Attach the formatter to the handler
-handler.setFormatter(formatter)
-# Attach the handler to the logger
-logger.addHandler(handler)
 
 # Make a class we can use to capture stdout and sterr in the log
 class MyLogger(object):
@@ -41,9 +21,9 @@ class MyLogger(object):
 			self.logger.log(self.level, message.rstrip())
  
 # Replace stdout with logging to file at INFO level
-#sys.stdout = MyLogger(logger, logging.INFO)
+sys.stdout = MyLogger(logger, logging.INFO)
 # Replace stderr with logging to file at ERROR level
-#sys.stderr = MyLogger(logger, logging.ERROR)
+sys.stderr = MyLogger(logger, logging.ERROR)
 
 	
 def readSerial():
@@ -105,22 +85,21 @@ def updateLogSerial(msgtype,dev,msg):
 	result=True
 	try:
 		db = sqlite3.connect(DB)
-                cur=db.cursor()
+		cur=db.cursor()
 		sqlstmt='''INSERT INTO logserial (TS,MSGTYPE,DEV,MSG) VALUES('%s','%s','%s','%s')''' % (datetime.now(),msgtype,dev,msg)
 		print sqlstmt
 		cur.execute(sqlstmt)
-                #cur.execute('''INSERT INTO logserial (TS,MSGTYPE,DEV,MSG) VALUES('?','?','?','?')''',(datetime.now(),msgtype,dev,msg))
-                db.commit()
-        except  sqlite3.Error, e:
-                print "Error: %s" % e.args[0]
-                result=False
-        except Exception, e:
-                print repr(e)
+		db.commit()
+	except  sqlite3.Error, e:
+		print "Error: %s" % e.args[0]
 		result=False
-        finally:
-                if db:
-                        db.close()
-                return result
+	except Exception, e:
+		print repr(e)
+		result=False
+	finally:
+		if db:
+			db.close()
+		return result
 
 def xbeeSend(cmd):
 	RETRIES=3
@@ -159,6 +138,27 @@ def parseSerial(line):
 	else:
 		updaResult=updateLogSerial('JNK','',line)
 
+
+ser = serial.Serial("/dev/ttyUSB0", baudrate=38400, timeout=1)
+xbee = ZigBee(ser,escaped=True)
+
+DB = 'dev.db'
+
+LOG_FILENAME = "/tmp/xbeelistener.log"
+LOG_LEVEL = logging.INFO # Could be e.g. "DEBUG" or "WARNING"
+
+logger = logging.getLogger(__name__)
+# Set the log level to LOG_LEVEL
+logger.setLevel(LOG_LEVEL)
+# Make a handler that writes to a file, making a new file at midnight and keeping 3 backups
+handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=3)
+# Format each log message like this
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+# Attach the formatter to the handler
+handler.setFormatter(formatter)
+# Attach the handler to the logger
+logger.addHandler(handler)
+
 while True:
 	lines = None
 	cmdRows = None
@@ -175,8 +175,8 @@ while True:
 			dev=cmdRow[1]
 			cmd=cmdRow[2]
 			if (xbeeSend(cmd)):
-				print 'Write to serial successful'
-				updResult=updateCmdStatus(ts,dev,cmd,'S')
+				print 'Write to serial successful and waiting for replay'
+				updResult=updateCmdStatus(ts,dev,cmd,'R')
 			else:
 				print 'Write to serial unsucessful'
 				updResult=updateCmdStatus(ts,dev,cmd,'E')
